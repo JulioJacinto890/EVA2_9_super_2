af55bb9224852d5cc80cf13cb1e839261914651a
JulioJacinto890 <124766374+JulioJacinto890@users.noreply.github.com>
2023-03-17 00:04:10 -0600

    Add files via upload

---
+++ EVA2_9_super_2/src/eva2_9_super_2/Estudiante.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_9_super_2;
+
+/**
+ *
+ * @author hp
+ */
+public class Estudiante extends Peronsa {
+    private String noControl;
+
+    public String getNoControl() {
+        return noControl;
+    }
+
+    public void setNoControl(String noControl) {
+        this.noControl = noControl;
+    }
+
+
+    
+    
+}
---
+++ EVA2_9_super_2/src/eva2_9_super_2/probedores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_9_super_2;
+
+/**
+ *
+ * @author hp
+ */
+public class probedores extends Peronsa {
+    private String rfc;
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+    
+}
